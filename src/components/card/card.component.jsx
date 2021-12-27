0af15e74ab83644d04dc79d3a722a7bb79f00255
Oscar Valdez <odvesims@gmail.com>
2021-12-27 15:05:59 -0400

    Fixed card component picture size

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = (props) => (
   <div className="card-container">
     <img
       alt="monster"
-      src={`https://robohash.org/${props.monster.id}?set=set2&size=160x180`}
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
     <h3>{props.monster.name}</h3>
     <p>{props.monster.email}</p>
